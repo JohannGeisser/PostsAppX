18a3a24bb7960a5e4226ff659fe8a9f1727bdaf5
JJGeisser <johann.geisser@training.assuresoft.com>
2023-03-13 11:48:26 -0400

    Adding data class for Comment Item

--- app/src/main/java/com/hyperskill/postsapp/domain/item/CommentItem.kt
+++ app/src/main/java/com/hyperskill/postsapp/domain/item/CommentItem.kt
@@ -1,3 +1,10 @@
 package com.hyperskill.postsapp.domain.item
 
-data class CommentItem()
+data class CommentItem(
+
+    val id: Int,
+    val name: String,
+    val body: String,
+    val email: String,
+    val postId: Int
+)
